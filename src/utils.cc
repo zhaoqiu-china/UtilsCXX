4f953c3f73c225642b5d300c1fbee850c2f80dd9
zhaoqiu <shengzhaoqiu18@mails.ucas.edu.cn>
2022-12-31 21:41:15 +0800

    first upload

---
+++ src/utils.cc
@@ -0,0 +1,15 @@
+#include <string>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#ifdef _WIN32
+std::size_t count_files(const std::string &folder)
+{
+    size_t counter = 0;
+    WIN32_FIND
+}
+#else
+
+
+#endif
