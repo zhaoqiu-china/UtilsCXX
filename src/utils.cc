2f4efc88ca474e7dcf99f6e3a7aff4bc7eb57f8a
zhaoqiu <shengzhaoqiu18@mails.ucas.edu.cn>
2022-12-31 22:21:38 +0800

    add fun

--- src/utils.cc
+++ src/utils.cc
@@ -1,4 +1,5 @@
 #include <string>
+#include <stdexcept>
 #ifdef _WIN32
 #include <windows.h>
 #endif
@@ -7,9 +8,29 @@
 std::size_t count_files(const std::string &folder)
 {
     size_t counter = 0;
-    WIN32_FIND
+    WIN32_FIND_DATAA ffd;
+
+    // Start iterating over the files in the folder directory.
+    HANDLE hFind = ::FindFirstFileA((folder + "\\*").c_str(), &ffd);
+    if (hFind != INVALID_HANDLE_VALUE)
+    {
+        do
+        {
+            if (ffd.cFileName[0] != '.')
+                counter++;
+            
+        } while (::FindNextFileA(hFind, &ffd) != 0);
+        ::FindClose(hFind);
+    }
+    else
+    {
+        throw std::runtime_error("Failed open folder " + folder);
+    }
+
+    return counter;
+    
 }
 #else
-
+std::size_t 
 
 #endif
